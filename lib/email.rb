d3e9d4bba11e90dd2caae04adb0692229a8d1745
Jonathan Camenisch <jonathan@camenisch.net>
2012-08-16 19:47:25 -0500

    Get email working (more or less)

--- lib/email.rb
+++ lib/email.rb
@@ -1,16 +1,22 @@
 class Email
   def initialize(settings, request)
-    @settings = settings.symbolize_keys
-    @settings[:via] = @settings[:via].to_sym
     @request = request
+
+    # Convert keys (and some values) to symbols for Pony's use
+    # With settings in YAML, it's more user-friendly to initialize them as strings
+    @settings = settings.symbolize_keys
+    @settings[:via_options] = @settings[:via_options].andand.symbolize_keys
+    @settings[:via] = @settings[:via].to_sym
+    @settings[:referer] = request.referer
+    @settings[:ip] = request.ip
   end
 
-  attr_reader :settings
+  attr_reader :settings, :request
 
   def send
     settings[:headers] ||= {}
-    settings[:headers]['X-X-Sender'] = "Message posted on #{@request.referer} from #{@request.ip}"
-    settings[:subject] ||= settings[:headers]['X-X-Sender']
+    settings[:headers]['X-X-Sender'] = "Message posted on #{request.referer} from #{request.ip}"
+    settings[:subject] = settings[:subject] ? Templater.parse(settings, settings[:subject]) : settings[:headers]['X-X-Sender']
 
     Pony.mail(settings)
   end
